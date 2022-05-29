b9b811e44fc534213134093b1fe9feef25e0c7b1
Herman van Houtum <hhoutum@doeit.net>
2022-05-29 13:52:48 +0200

    added traviskeys

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Het is 15 mei.
+          Het is 29 mei.
         </p>
         <a
           className="App-link"
